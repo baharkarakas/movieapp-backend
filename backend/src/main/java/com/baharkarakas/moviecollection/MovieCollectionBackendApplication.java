169052d963e6ba1f61a759fddca51490352616ef
Bahar Karakaş <baharkarakas03@gmail.com>
2025-12-15 14:01:45 +0300

    Initial commit (new project)

---
+++ backend/src/main/java/com/baharkarakas/moviecollection/MovieCollectionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.baharkarakas.moviecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCollectionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCollectionBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/baharkarakas/moviecollection/MovieCollectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baharkarakas.moviecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCollectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
